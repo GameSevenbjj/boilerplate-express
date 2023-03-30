f1d9cbebe21d27937221c33172bb8a9cedd70067
GameSevenbjj <rueben.beard@gmail.com>
2023-03-30 18:32:28 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,17 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+console.log("Hello World");
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use(express.static(__dirname));
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
